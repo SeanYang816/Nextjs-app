3b2aa0617ca547ea3c1147cec1244236febf4512
seanyang816 <seanyang816@gmail.com>
2023-06-14 13:44:46 +0800

    feat: create profile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,119 +1,35 @@
 import Head from 'next/head';
-import styles from './index.module.scss';
-import Link from 'next/link';
+import Layout, { siteTitle } from '../components/Layout/Layout';
+import utilStyles from '../styles/utils.module.scss';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-
-      <main>
-      <h1 className={styles.title}>
-          Read <a href="/posts/first-post">this page!</a>
-
-          Read <Link href="/posts/first-post">this page!</Link>
-        </h1>
-
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Greetings, everyone! My name is Anya Forger, and I'm delighted to share a unique self-introduction with you. Picture a six-year-old girl with vibrant pink hair, filled with curiosity and wonder. But there's something extraordinary about me that I must keep as a closely guarded secret—I possess telepathic powers!
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <p>
+          While my appearance may be ordinary, my telepathic abilities allow me to communicate with others using the power of the mind. It's an incredible gift that I've kept hidden from everyone around me. Whether it's sensing someone's thoughts, sending messages silently, or even delving into the depths of their minds, my telepathy grants me a remarkable connection with the world.
+        </p>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <p>
+          To maintain the secrecy of my telepathic power, I'm careful not to reveal my abilities to anyone. It's a personal journey, filled with self-discovery and the responsibility to use my powers ethically and for the greater good. I'm constantly learning to control and harness my telepathic abilities, navigating the intricate web of thoughts and emotions that surround us all.
+        </p>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        <p>
+          While my telepathy may set me apart, I'm just like any other child, curious about the world, eager to learn, and longing to make genuine connections. So, as we embark on this interaction together, let's explore the depths of knowledge, engage in captivating conversations, and embrace the wonder and potential that lies within each of us.
+        </p>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        <p>
+          Remember, my telepathic abilities remain our little secret, allowing us to embark on a unique journey of understanding and connection. I'm here to assist you in any way I can, telepathic or otherwise, so don't hesitate to ask me anything or share your thoughts. Let's embark on an extraordinary adventure together!
+        </p>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+      </section>
+    </Layout>
+  );
 }
