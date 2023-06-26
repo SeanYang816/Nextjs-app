9ecf383f4587b68a25f8881e1e36ce32ff177ff0
seanyang816 <seanyang816@gmail.com>
2023-06-26 13:42:37 +0800

    fix: date

--- src/lib/posts.tsx
+++ src/lib/posts.tsx
@@ -4,29 +4,26 @@ import matter from 'gray-matter';
 import { remark } from 'remark';
 import html from 'remark-html';
 
+interface MyObject {
+  id: string;
+  date?: string; // Make the 'date' property optional or adjust its type as needed
+}
+
 const postsDirectory = path.join(process.cwd(), 'src/posts');
 
-export function getSortedPostsData() {
-  // Get file names under /posts
+export function getSortedPostsData(): MyObject[] {
   const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
+  const allPostsData: MyObject[] = fileNames.map((fileName) => {
     const id = fileName.replace(/\.md$/, '');
-
-    // Read markdown file as string
     const fullPath = path.join(postsDirectory, fileName);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-    // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents);
-
-    // Combine the data with the id
     return {
       id,
       ...matterResult.data,
     };
   });
-  // Sort posts by date
+
   return allPostsData.sort((a, b) => {
     if (a.date < b.date) {
       return 1;
